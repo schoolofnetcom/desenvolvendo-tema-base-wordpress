40301ad76ddb9b54285a6148a266853a3e1c0228
thivalls <olavalls@gmail.com>
2020-11-19 17:42:38 -0300

    Aula 3 - Finalizando embiente de desenvolvimento laravel-mix

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/app.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Hello Base Theme</h1>
+        <button class="btn btn-primary">Button</button>
+    </div>
+
+    <script src="./assets/js/app.js"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js('src/js/app.js', 'assets/js')
+    .sass('src/sass/app.scss', 'assets/css', [
         //
     ]);
